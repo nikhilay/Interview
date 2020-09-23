dfe3b85a7cf06226debe43b8234b6500f66ee077
nikhilay <nikhilayyagari15@gmail.com>
2020-09-23 01:25:50 -0700

    [LC] Move Zeroes

---
+++ interview2020/src/leetcode/easy/MoveZeroes.java
@@ -0,0 +1,23 @@
+package leetcode.easy;
+
+public class MoveZeroes {
+    
+//https://leetcode.com/problems/move-zeroes/discuss/72011/Simple-O(N)-Java-Solution-Using-Insert-Index
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return;
+        }
+        int pivot = 0;
+        for(int num: nums){
+            if(num!=0){
+                nums[pivot++] = num;
+            }
+        }
+
+        while ( pivot< nums.length){
+            nums[pivot++] = 0;
+        }
+
+
+    }
+}
