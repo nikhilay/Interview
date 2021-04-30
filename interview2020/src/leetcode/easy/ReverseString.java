86784e466f89032f7fb2a67c459bacc0c418f0ed
nikhilay <nikhilayyagari15@gmail.com>
2021-04-29 23:50:23 -0700

    344. Reverse String

---
+++ interview2020/src/leetcode/easy/ReverseString.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int  start = 0;
+        int end = s.length - 1;
+
+        while(start <= end){
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+
+    }
+}
