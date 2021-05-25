b5ff1b6fbfa73b68068418bb9fe9cd80bb0209e8
nikhilay <nikhilayyagari15@gmail.com>
2021-05-25 16:22:01 -0700

    191. Number of 1 Bits

---
+++ interview2020/src/leetcode/easy/NumberOf1Bits.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+}
