5b63d0c833ebff84cb11670c54dfedf7eb6e4b72
nikhilay <nikhilayyagari15@gmail.com>
2021-05-28 16:23:57 -0700

    415. Add Strings

---
+++ interview2020/src/leetcode/easy/AddStrings.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += num1.charAt(i) - '0';
+            i--;
+            if (j >= 0) sum += num2.charAt(j) - '0';
+            j--;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            sb.append(digit);
+        }
+        if(carry!=0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
