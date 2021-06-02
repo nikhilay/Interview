f91b2852a78e0529e918b93ee1ace0724edd397d
nikhilay <nikhilayyagari15@gmail.com>
2021-06-02 14:42:39 -0700

    102. Binary Tree Level Order Traversal

---
+++ interview2020/src/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+import leetcode.easy.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        levelOrder(root, result, 0);
+        return result;
+    }
+
+    public void levelOrder(TreeNode root, List<List<Integer>> result, int level) {
+        if (root == null) return;
+        if (result.size() == level) {
+            result.add(new ArrayList<Integer>());
+        }
+        result.get(level).add(root.val);
+        levelOrder(root.left, result, level + 1);
+        levelOrder(root.right, result, level + 1);
+    }
+}
