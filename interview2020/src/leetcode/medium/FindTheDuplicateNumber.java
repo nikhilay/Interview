34cf50ec3a63504066370fe519f5be78b1f3f54e
nikhilay <nikhilayyagari15@gmail.com>
2021-06-05 16:22:22 -0700

    287. Find the Duplicate Number

---
+++ interview2020/src/leetcode/medium/FindTheDuplicateNumber.java
@@ -0,0 +1,21 @@
+package leetcode.medium;
+
+public class FindTheDuplicateNumber {
+    //https://leetcode.com/problems/find-the-duplicate-number/solution/
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
