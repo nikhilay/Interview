13a9978c81c0c2e326bb32b3ade887b3b0fb85d6
nikhilay <nikhilayyagari15@gmail.com>
2021-06-11 19:14:26 -0700

    11. Container With Most Water

---
+++ interview2020/src/leetcode/medium/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package leetcode.medium;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
