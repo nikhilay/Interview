1728d8006799e777e439d4d3f369b315a694e45e
nikhilay <nikhilayyagari15@gmail.com>
2021-06-14 17:20:15 -0700

    1209. Remove All Adjacent Duplicates in String II

---
+++ interview2020/src/leetcode/medium/RemoveAllAdjacentDuplicatesInStringII.java
@@ -0,0 +1,26 @@
+package leetcode.medium;
+
+public class RemoveAllAdjacentDuplicatesInStringII {
+    //https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/discuss/392933/JavaC%2B%2BPython-Two-Pointers-and-Stack-Solution
+    public String removeDuplicates(String s, int k) {
+        if (k == 0) return s;
+        int len = s.length();
+        int[] count = new int[len];
+        int[] uniqueString = new int[len];
+
+        int i = 0;
+        for (int j = 0; j < len && i < len; j++, i++) {
+            uniqueString[i] = s.charAt(j);
+
+            if (i > 0 && uniqueString[i - 1] == s.charAt(j)) {
+                count[i] = count[i - 1] + 1;
+
+            } else {
+                count[i] = 1;
+            }
+            if (count[i] == k) i -= k;
+        }
+
+        return new String(uniqueString, 0, i);
+    }
+}
