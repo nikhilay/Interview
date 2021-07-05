e9d1f54e701c730d254953f74795f5a6ddb559d3
nikhilay <nikhilayyagari15@gmail.com>
2021-07-04 18:53:07 -0700

    189. Rotate Array

---
+++ interview2020/src/leetcode/medium/RotateArray.java
@@ -0,0 +1,24 @@
+package leetcode.medium;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(0,nums.length -1, nums);
+        reverse(0,k-1, nums);
+        reverse(k,nums.length -1, nums);
+
+
+    }
+
+    private void reverse(int start, int end, int[] nums) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+}
