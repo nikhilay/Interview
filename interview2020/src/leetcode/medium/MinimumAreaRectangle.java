bb0731aa4f50563fa89d7087e46fb37b46c25918
nikhilay <nikhilayyagari15@gmail.com>
2021-10-02 18:31:05 -0700

    939. Minimum Area Rectangle

---
+++ interview2020/src/leetcode/medium/MinimumAreaRectangle.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class MinimumAreaRectangle {
+    //https://leetcode.com/problems/minimum-area-rectangle/discuss/192025/Java-N2-Hashmap
+    public int minAreaRect(int[][] points) {
+        Map<Integer, HashSet<Integer>> map = new HashMap<>();
+        for (int[] p : points) {
+            if (!map.containsKey(p[0])) map.put(p[0], new HashSet<>());
+            map.get(p[0]).add(p[1]);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int[] p1 : points) {
+            for (int[] p2 : points) {
+                if (p1[0] == p2[0] || p1[1] == p2[1]) continue;
+                if (map.get(p1[0]).contains(p2[1]) && map.get(p2[0]).contains(p1[1])) {
+                    min = Math.min(min, Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]));
+                }
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+
+    }
+}
