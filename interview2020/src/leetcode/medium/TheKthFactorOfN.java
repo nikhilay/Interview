189ae11976c4313f0224a336b3db6feaf36cf1d8
nikhilay <nikhilayyagari15@gmail.com>
2021-11-02 23:53:02 -0700

    1492. The kth Factor of n

---
+++ interview2020/src/leetcode/medium/TheKthFactorOfN.java
@@ -0,0 +1,13 @@
+package leetcode.medium;
+
+public class TheKthFactorOfN {
+
+    public int kthFactor(int n, int k) {
+        for (int i = 1; i <= n/2 && k>0; i++) {
+            if (n % i == 0 && --k==0) {
+                return i;
+            }
+        }
+        return k == 1 ? n : -1;
+    }
+}
