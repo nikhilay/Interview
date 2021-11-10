9ab24424c8fcb0ddb791fd118b0dc03771b14765
nikhilay <nikhilayyagari15@gmail.com>
2021-11-10 08:26:38 -0800

    improved

--- interview2020/src/leetcode/medium/IntegerBreak.java
+++ interview2020/src/leetcode/medium/IntegerBreak.java
@@ -1,17 +1,13 @@
 package leetcode.medium;
 
 public class IntegerBreak {
-    //https://leetcode.com/problems/integer-break/discuss/80694/Java-DP-solution/85260
+    //https://leetcode.com/problems/integer-break/discuss/80694/Java-DP-solution/
     public int integerBreak(int n) {
         int[] dp = new int[n + 1];
         dp[1] = 1;
-        for (int i = 1; i < n; i++) {
-            dp[i] = i;
-        }
-
         for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i / 2; j++) {
-                dp[i] = Math.max(dp[i], dp[i - j] * dp[j]);
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
             }
         }
         return dp[n];
